14fa48f4ae12699e8aaa91ddbc1a2a9936e47d96
Liisi V <liisepp@tktk.ee>
2022-09-18 15:11:54 +0300

    add week2 code

---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//rakendus küsib sugu
+//rakendus küsib perekonnanime
+//raekndus tervitab kasutajat järgmiselt: 
+//"Welcome Mr. Perekonnanimi" Või "Welcome Ms. Perekonnanimi"
+
+Console.WriteLine("Please select your gender (m/f):");
+char userGender = Char.Parse(Console.ReadLine());
+Console.WriteLine("Please enter your last name:");
+string userLastName = Console.ReadLine();
+
+if(userGender == 'm') 
+{
+    Console.WriteLine($"Welcome, Mr.{userLastName}!");
+}
+else if(userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms.{userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, !");
+}
